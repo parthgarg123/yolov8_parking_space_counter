ac7627f0a84e5d20e647112e7943600d0a2a823b
parthgarg123 <88800472+parthgarg123@users.noreply.github.com>
2024-03-07 15:57:04 +0530

    first

---
+++ data/test2.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import pickle
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+
+
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+
+model=YOLO('yolov8s.pt')
+
+
+cap=cv2.VideoCapture()
+
+
+
+
+count=0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        continue
+   
+    count += 1
+    if count % 3 != 0:
+       continue
+
+    frame=cv2.resize(frame,(1020,500))
+    frame_copy = frame.copy()
+    results=model.predict(frame)
+ #   print(results)
+    a=results[0].boxes.data
+    px=pd.DataFrame(a).astype("float")
+#    print(px)
+    
+    for index,row in px.iterrows():
+#        print(row)
+ 
+        x1=int(row[0])
+        y1=int(row[1])
+        x2=int(row[2])
+        y2=int(row[3])
+        d=int(row[5])
+        
+        c=class_list[d]
+        cx=int(x1+x2)//2
+        cy=int(y1+y2)//2
+        
+     
+              
+    cv2.imshow('FRAME', frame)
+    key = cv2.waitKey(1) & 0xFF
+
+
+cap.release()
+
+cv2.destroyAllWindows()
+
